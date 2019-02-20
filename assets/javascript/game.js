40489f37ba15acbea4465495dfbd87528b8f4394
mchlgrca <mgarcia0898@gmail.com>
2019-02-19 19:34:33 -0700

    background image and variables added

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+var crystals = ["red", "blue", "green", "yellow"];
+var score = 0;
+
+var winCount = 0;
+var lossCount = 0;
+
+$(document).ready(function() {
+
+    $()
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
