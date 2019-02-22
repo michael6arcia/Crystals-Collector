875462ba4fc6b598ac18933ce0769380f65a150e
mchlgrca <mgarcia0898@gmail.com>
2019-02-21 21:10:40 -0700

    number generation improved

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,16 +6,27 @@ var totalScore = 0;
 var winCount = 0;
 var lossCount = 0;
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-    var startNum = Math.floor(Math.random() * 120);
-        $("#startNum").text(startNum);
+    // Display stats, starting number, and total score.
+
+    var startNum = Math.floor(Math.random() * 120) + 19;
+    $("#startNum").text(startNum);
 
     $("#totalScore").text(totalScore);
 
     $("#winCount").text(winCount);
 
     $("#lossCount").text(lossCount);
+
+    // Click functions for gems.
+
+    for (var i = 0; i < crystals.length; i++) {
+        i = Math.floor(Math.random() * 12) + 1;
+    }
+
+
+
 
 
 
