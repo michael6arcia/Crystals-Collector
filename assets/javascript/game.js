8b318bf2fdb9e7358fab91511361f7ad9609276e
mchlgrca <mgarcia0898@gmail.com>
2019-02-23 12:58:46 -0700

    reset function added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var crystals = ["red", "blue", "yellow", "purple"];
 
-var startNum = "";
+var startNum = Math.floor(Math.random() * 120) + 19;;
 var totalScore = 0;
 
 var winCount = 0;
@@ -10,34 +10,76 @@ $(document).ready(function () {
 
     // Display stats, starting number, and total score.
 
-    var startNum = Math.floor(Math.random() * 120) + 19;
     $("#startNum").text(startNum);
-
     $("#totalScore").text(totalScore);
-
     $("#winCount").text(winCount);
-
     $("#lossCount").text(lossCount);
 
     // Random number assignment for gems.
 
     for (var i = 0; i < crystals.length; i++) {
-        crystals[i] = Math.floor(Math.random() * 12) + 1;
+        crystals[i] = Math.floor(Math.random() * 11) + 1;
+        var redValue = crystals[0];
+        var blueValue = crystals[1];
+        var yellowValue = crystals[2];
+        var purpleValue = crystals[3];
     }
 
     // Click function assignment for gems.
 
+    $("#red").on("click", function () {
+        totalScore += redValue;
+        $("#totalScore").text(totalScore);
+    });
+
+    $("#blue").on("click", function () {
+        totalScore += blueValue;
+        $("#totalScore").text(totalScore);
+    });
+
+    $("#yellow").on("click", function () {
+        totalScore += yellowValue;
+        $("#totalScore").text(totalScore);
+    });
+
+    $("#purple").on("click", function () {
+        totalScore += purpleValue;
+        $("#totalScore").text(totalScore);
+    });
+
     // Winning Conditional
 
-    if (totalScore == startNum) {
+    if (totalScore === startNum) {
         winCount++;
+        $("#winCount").text(winCount);
+        resetGame();
     }
 
     // Losing Conditional
 
-    if (totalScore >= startNum) {
+    else if (totalScore >= startNum) {
         lossCount++;
+        $("#lossCount").text(lossCount);
+        resetGame();
     }
+
+    // Reset Function
+
+    function resetGame() {
+        // Starting number and total score reset.
+        var startNum = Math.floor(Math.random() * 120) + 19;;
+        var totalScore = 0;
+
+        // Crystal value reassignment.
+        for (var i = 0; i < crystals.length; i++) {
+            crystals[i] = Math.floor(Math.random() * 11) + 1;
+            var redValue = crystals[0];
+            var blueValue = crystals[1];
+            var yellowValue = crystals[2];
+            var purpleValue = crystals[3];
+        }
+    }
+    resetGame();
 
 
 
